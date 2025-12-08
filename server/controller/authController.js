22b3de81c42ee6d0a67bc9888fe6e45f065b286a
Ramenttharu <rametntharu18@gmail.com>
2025-12-09 00:22:18 +0545

    initial commit

---
+++ server/controller/authController.js
@@ -0,0 +1,13 @@
+export const register = async (req,res) =>{
+
+    const {name, email, password} = req.body
+
+    if(!name || !email || !password){
+        return res.json({success: false, message: 'missing Details'})
+    }
+    try {
+        
+    } catch (error) {
+        
+    }
+}
