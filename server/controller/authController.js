0bfe3337c501b13191c5b93d399f24600d61bb4f
Ramenttharu <rametntharu18@gmail.com>
2025-12-09 15:34:06 +0545

    update

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,3 +1,8 @@
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import userModel from '../model/userModel.js'
+
+
 export const register = async (req,res) =>{
 
     const {name, email, password} = req.body
@@ -6,8 +11,84 @@ export const register = async (req,res) =>{
         return res.json({success: false, message: 'missing Details'})
     }
     try {
+
+        const existingUser = await userModel.findOne({email})
+
+        if(existingUser){
+            return res.json({success: false, message: "User already exist"});   
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new userModel({
+            name, 
+            email, 
+            password: hashedPassword
+        });
+        await user.save()
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+        return res.json({success: true});
+
         
     } catch (error) {
+        
+        return res.json({success: false, massage: error.message});
+        
+    }
+}
+
+
+export const login = async (req,res) => {
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.json({success: false, message: 'Email and password are required'})
+    }
+    try {
+        const user = await userModel.findOne({email});
+
+        if(!user){
+            return res.json({success: false, massage: 'Invalid email'})
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+             return res.json({success: false, massage: 'Invalid password'})
+        }
+         const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+        return res.json({success: true});
+        
+    } catch (error) {
+          return res.json({success: false, massage: error.message})
+        
+    }
+}
+
+export const logout = async(req, res) => {
+    try {
+        res.clearCookie('token', {
+             httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        return res.json({success: true, message:'Logged Out'})
+        
+    } catch (error) {
+         return res.json({success: false, massage: error.message})
         
     }
 }
